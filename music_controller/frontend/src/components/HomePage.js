e0fbf6fc4b8ff86d9a63b4cc55bf0b91ef0b8535
astelwilliam <astelwilliam11@gmail.com>
2025-04-15 15:06:48 +0530

    Commit changes to music_controller project

---
+++ music_controller/frontend/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the HomePage</h1>
+      <p>This is a placeholder HomePage component.</p>
+    </div>
+  );
+};
+
+export default HomePage;
